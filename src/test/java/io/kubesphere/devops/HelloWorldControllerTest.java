1c9eaf14b187968da979553cf139b3d40b4b3e5d
FeynmanZhou <zpf0610@gmail.com>
2020-02-22 10:38:32 +0800

    fix typo
    Signed-off-by: FeynmanZhou <zpf0610@gmail.com>

--- src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
+++ src/test/java/io/kubesphere/devops/HelloWorldControllerTest.java
@@ -8,6 +8,6 @@ public class HelloWorldControllerTest {
 
     @Test
     public void testSayHello() {
-        assertEquals("Really appreaciate your star, that's the power of our life.", new HelloWorldController().sayHello());
+        assertEquals("Really appreciate your star, that's the power of our life.", new HelloWorldController().sayHello());
     }
 }
