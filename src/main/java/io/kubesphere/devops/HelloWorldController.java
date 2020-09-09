07b49c01db39e0fd0d04b2b73566ad2b80217c70
JaredTan95 <jian.tan@daocloud.io>
2020-09-09 09:18:39 +0800

    add hello api.

--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -2,6 +2,7 @@ package io.kubesphere.devops;
 
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class HelloWorldController {
     @RequestMapping("/")
     public String sayHello() {
         return "Really appreciate your star, that's the power of our life.";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello.";
     }
 }
